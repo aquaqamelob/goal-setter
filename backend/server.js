e99c19e9b8f4d5e039e6af0915987c2f41e40a1f
norbertzieba <61473367+norbertzieba@users.noreply.github.com>
2022-10-03 19:23:37 +0200

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", goalRoutes);
 app.use("/api/users", userRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
 app.use(errorHandler);
 app.listen(port, () => {
   console.log("server is listening at http://localhost:" + port);
