2422a10e51c41cdc5d180dd214c15c1471eec478
aquaqamelob <61473367+aquaqamelob@users.noreply.github.com>
2023-01-06 13:49:17 +0100

    Delete frontend directory

--- frontend/src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice";
-import goalReducer from "../features/goals/goalSlice";
-export default configureStore({
-  reducer: { auth: authReducer, goals: goalReducer },
-});
